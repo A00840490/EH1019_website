a5d6a1ed55f7a896a903475df15a7037f180a162
A00840490 <a00840490@tec.mx>
2025-12-03 08:16:05 -0600

    deploy workflo

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '',
+  base: "/EH1019_website/",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
